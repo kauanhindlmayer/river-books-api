9b8bafd156683acbef9903b3507670a04ef30e70
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2024-08-11 13:46:48 -0300

    implementing fluent validations

--- src/RiverBooks.Books/Endpoints/UpdateBookPrice/UpdateBookPriceRequest.cs
+++ src/RiverBooks.Books/Endpoints/UpdateBookPrice/UpdateBookPriceRequest.cs
@@ -1,3 +1,21 @@
+using FastEndpoints;
+using FluentValidation;
+
 namespace RiverBooks.Books.Endpoints.UpdateBookPrice;
 
-internal record UpdateBookPriceRequest(Guid Id, decimal Price);
+public record UpdateBookPriceRequest(Guid Id, decimal Price);
+
+public class UpdateBookPriceRequestValidator : Validator<UpdateBookPriceRequest>
+{
+    public UpdateBookPriceRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .NotEqual(Guid.Empty)
+            .WithMessage("Id must not be empty.");
+
+        RuleFor(x => x.Price)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Price must be greater than or equal to 0.");
+    }
+}
